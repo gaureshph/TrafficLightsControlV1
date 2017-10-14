7858448c2c65767d86bf9bc28dff90412506bbb3
Gouresh Phadke <gauresh.phadke@yahoo.com>
2017-10-14 16:51:58 +0530

    Added domain models for junctions and signals.

---
+++ TrafficLightsControlV1.Web/DomainModels/TrafficSignal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using TrafficLightsControlV1.Web.Enums;
+
+namespace TrafficLightsControlV1.Web.DomainModels
+{
+    public class TrafficSignal
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [ForeignKey("TrafficJunction")]
+        public int TrafficJunction_Id { get; set; }
+
+        public virtual TrafficJunction TrafficJunction { get; set; }
+
+        public ExitDirection ExitDirection { get; set; }
+
+        public bool IsGreen { get; set; }
+    }
+}
---
+++ TrafficLightsControlV1.Web/Enums/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrafficLightsControlV1.Web.Enums
+{
+    public enum ExitDirection
+    {
+        EXITUP,
+        EXITRIGHT,
+        EXITDOWN,
+        EXITLEFT
+    }
+}
