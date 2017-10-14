c0690df5ca91e8cc46c853eb411ac61076aee186
Gouresh Phadke <gauresh.phadke@yahoo.com>
2017-10-14 18:18:10 +0530

    Traffic Junction CRUD done.

--- TrafficLightsControlV1.Web/Controllers/TrafficJunctionController.cs
+++ TrafficLightsControlV1.Web/Controllers/TrafficJunctionController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TrafficLightsControlV1.Web.DbContexts;
+using TrafficLightsControlV1.Web.DomainModels;
 
 namespace TrafficLightsControlV1.Web.Controllers
 {
@@ -29,6 +30,12 @@ namespace TrafficLightsControlV1.Web.Controllers
             try
             {
                 // TODO: Add insert logic here
+                _context.TrafficJunctions.Add(new TrafficJunction
+                {
+                    Id = Convert.ToInt32(collection["Id"]),
+                    Name = collection["Name"]
+                });
+                _context.SaveChanges();
 
                 return RedirectToAction("Index");
             }
@@ -41,7 +48,7 @@ namespace TrafficLightsControlV1.Web.Controllers
         // GET: TrafficJunction/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            return View(_context.TrafficJunctions.FirstOrDefault(lmb => lmb.Id == id));
         }
 
         // POST: TrafficJunction/Edit/5
@@ -51,7 +58,12 @@ namespace TrafficLightsControlV1.Web.Controllers
             try
             {
                 // TODO: Add update logic here
-
+                _context.Entry(new TrafficJunction
+                {
+                    Id = Convert.ToInt32(collection["Id"]),
+                    Name = collection["Name"]
+                }).State = System.Data.Entity.EntityState.Modified;
+                _context.SaveChanges();
                 return RedirectToAction("Index");
             }
             catch
@@ -62,8 +74,8 @@ namespace TrafficLightsControlV1.Web.Controllers
 
         // GET: TrafficJunction/Delete/5
         public ActionResult Delete(int id)
-        {
-            return View();
+        {        
+            return View(_context.TrafficJunctions.FirstOrDefault(lmb => lmb.Id == id));
         }
 
         // POST: TrafficJunction/Delete/5
@@ -73,12 +85,18 @@ namespace TrafficLightsControlV1.Web.Controllers
             try
             {
                 // TODO: Add delete logic here
+                _context.Entry(new TrafficJunction
+                {
+                    Id = id
+                }).State = System.Data.Entity.EntityState.Deleted;
+                _context.SaveChanges();
 
                 return RedirectToAction("Index");
             }
-            catch
+            catch(Exception ex)
             {
                 return View();
+                throw ex;
             }
         }
 
